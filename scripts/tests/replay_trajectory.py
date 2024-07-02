571cfe8e75f5acddea825659ebe0622f9122e92d
Kyle Hsu <kyle.hsu@tri.global>
2024-07-02 15:42:59 -0700

    playback tests

--- scripts/tests/replay_trajectory.py
+++ scripts/tests/replay_trajectory.py
@@ -1,8 +1,19 @@
 from droid.robot_env import RobotEnv
 from droid.trajectory_utils.misc import replay_trajectory
 
-trajectory_folderpath = "/home/sasha/DROID/data/success/2023-02-16/Thu_Feb_16_16:27:00_2023"
-action_space = "joint_position"
+# pick up can
+# trajectory_folderpath = "/home/ashwinbalakrishna/kylehsu/code/fix-droid/droid/data/success/2024-07-01/Mon_Jul__1_23:33:27_2024"
+
+# pick up teaspoon (by bowl)
+# trajectory_folderpath = "/home/ashwinbalakrishna/kylehsu/code/fix-droid/droid/data/success/2024-07-02/Tue_Jul__2_12:59:19_2024"
+
+# pick up teaspoon (by handle)
+trajectory_folderpath = "/home/ashwinbalakrishna/kylehsu/code/fix-droid/droid/data/success/2024-07-02/Tue_Jul__2_12:59:49_2024"
+
+# action_space = "joint_position"
+# action_space = "cartesian_position"
+# action_space = "joint_velocity"
+action_space = "cartesian_velocity"
 
 # Make the robot env
 env = RobotEnv(action_space=action_space)
